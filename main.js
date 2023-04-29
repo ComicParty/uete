dd1b8d4230fa1c00f803862e623e5caa96eabcb0
ComicParty <angerasura@icloud.com>
2023-04-29 16:30:21 +0800

    视频导航网站收工

--- main.js
+++ main.js
@@ -1,32 +1,72 @@
-const hashMap=[
-    {logo:'A',url:'https://www.aliali.cn/'},
-    {
-        logo: 'B',
-        url:'https://www.bilibili.com/'
-    },
-    {
-        logo: 'C',
-        url:'https://clicli.com.cn/'
-    }
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    {logo: 'A', url: 'https://www.acfun.cn'},
+    {logo: 'B', url: 'https://www.bilibili.com'},
+    {logo: 'D', url: 'https://douyin.com'},
+    {logo: 'I', url: 'https://www.iqiyi.com/'},
+    {logo: 'K', url: 'https://www.kuaishou.com/cn'},
+    {logo: 'Q', url: 'https://v.qq.com/'},
+    {logo: 'T', url: 'https://tudou.com/'},
+    {logo: 'Y', url: 'https://www.youku.com'}
 ]
-$('.addButton')
-    .on('click',()=>{
-            let url = window.prompt('请问你要添加的网址是啥？')
-            if(url.indexOf('http')!=0){
-                    url='https://'+url
-            }
-            const $siteList =$('.siteList')
-            const $lastLi = $siteList.find('li.last')
-            const $li = $(
-                `<li>
-                    <a href="${url}">
-                        <div class="site">
-                            <div class="logo">
-                                ${url[0]}
-                            </div>
-                            <div class="link">${url}</div>
-                        </div>
-                    </a>
-                </li>`
-            ).insertBefore($lastLi)
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+      <div class="site">
+        <div class="logo">${node.logo}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <img src="images/del.png" class="icon">
+        </div>
+      </div>
+    </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()
+            hashMap.splice(index, 1)
+            render()
+        })
     })
+}
+
+render()
+
+$('.addButton').on('click', () => {
+    let url = window.prompt('请输入您要添加的网址：')
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+    }
+    console.log(url)
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+    })
+    render()
+})
+
+window.onbeforeunload = () => {
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
+
+$(document).on('keypress', (e) => {
+    const {key} = e
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
